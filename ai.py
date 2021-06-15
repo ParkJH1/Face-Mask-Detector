c8ad8a548d785298d02e5927c68418cfd1b811d4
박정현 <pjh9996@naver.com>
2021-06-15 16:48:32 +0900

    Update ai.py

--- ai.py
+++ ai.py
@@ -1,5 +1,13 @@
 # ai.py
 import tensorflow as tf
+import numpy as np
+import os
+
+resize_and_crop = tf.keras.Sequential([
+    tf.keras.layers.experimental.preprocessing.RandomCrop(height=224, width=224),
+    tf.keras.layers.experimental.preprocessing.Rescaling(1. / 255)
+])
+
 
 # 학습 데이터 로드
 def load_data():
@@ -21,20 +29,23 @@ def load_data():
         batch_size=16
     )
 
-    resize_and_crop = tf.keras.Sequential([
-        tf.keras.layers.experimental.preprocessing.RandomCrop(height=224, width=224),
-        tf.keras.layers.experimental.preprocessing.Rescaling(1. / 255)
-    ])
-
     rc_train_dataset = train_dataset.map(lambda x, y: (resize_and_crop(x), y))
     rc_valid_dataset = train_dataset.map(lambda x, y: (resize_and_crop(x), y))
 
     return rc_train_dataset, rc_valid_dataset
 
 
+# 모델 학습
+def train_model(model, epochs, train_dataset, valid_dataset, save_model):
+    history = model.fit(train_dataset, epochs=epochs, validation_data=valid_dataset)
+    if save_model:
+        model.save('models/mymodel')
+    return history
+
+
 # 모델 생성
-def create_model(load_model):
-    if load_model:
+def create_model():
+    if os.path.exists('models/mymodel'):
         model = tf.keras.models.load_model('models/mymodel')
 
         model.layers[0].trainable = False
@@ -60,20 +71,16 @@ def create_model(load_model):
             optimizer=tf.keras.optimizers.RMSprop(lr=learning_rate),
             metrics=['accuracy']
         )
+
+        train_dataset, valid_dataset = load_data()
+        train_model(model, 2, train_dataset, valid_dataset, True)
     return model
-
-
-# 모델 학습
-def train_model(model, epochs, train_dataset, valid_dataset, save_model):
-    history = model.fit(train_dataset, epochs=epochs, validation_data=valid_dataset)
-    if save_model:
-        model.save('models/mymodel')
-    return history
 
 
 # 모델 예측
 def predict(model, image):
-    result = model.predict(image)
+    rc_image = resize_and_crop(np.array([image]))
+    result = model.predict(rc_image)
     if result[0] > 0.5:
         return 1
     else:
